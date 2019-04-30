26e42fd47f110c1c449ffc22eb01c576b393c564
malakmarey <45039969+malakmarey@users.noreply.github.com>
2019-04-30 22:39:48 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -32,6 +32,8 @@ int main()
 
 		testcase.setname("Calculate bull and pgia")
 			.CHECK_OUTPUT(calculateBullAndPgia("1234", "1234"), "4,0") // 4 bull, 0 pgia
+					.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
+
 			// TESTING BULL AND PGIA
 			.CHECK_OUTPUT(calculateBullAndPgia("5555", "5555"), "4,0") // 4 bull, 0 pgia
 			.CHECK_OUTPUT(calculateBullAndPgia("0000", "5555"), "0,0") // 0 bull, 0 pgia
@@ -43,7 +45,9 @@ int main()
 		testcase.setname("Play with dummy choosers and guessers")
 			.CHECK_EQUAL(play(c1234, g1234, 4, 100), 1)	
 			.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101) 
-			.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)   
+			.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)
+					.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
+   
 			;
 
 		testcase.setname("Play with smart guesser");
@@ -86,4 +90,3 @@ int main()
 	cout << "Your grade is: " << grade << endl;
 	return 0;
 }
-
