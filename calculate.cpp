836e3087ee3754996f319c1914a24403160dc6d8
malakmarey <45039969+malakmarey@users.noreply.github.com>
2019-05-05 16:28:23 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -1,43 +1,66 @@
-#include <iostream>
-using namespace std;
 #include "calculate.hpp"
-#include<string>
-using namespace std;
-namespace bullpgia{
 
-    string calculateBullAndPgia(string choice, string guess){
-        int i;
-		int j;
-		int pgiaa = 0; // good guess without index matchings
-		int bulls = 0; // good guess with index matchings as well
-		int num = choice.length();
-		int* arr_ZeroF_oneT = new int[num]; //represents the guess string , marks hits , regardless if it's a bull or pgiaa
-      //this loop should be dele
-		for (i = 0; i < num; i++) {
-			arr_ZeroF_oneT[i] = 0;
-		}
+using std::string, std::to_string ,std::fill ;
 
-		for (i = 0; i < choice.length(); i++) {
-			char c = choice.at(i);
-			char g = guess.at(i);
-			if (c == g) {
-				bulls++;
-				arr_ZeroF_oneT[i] = 1;
-				continue;
+using namespace bullpgia;
+
+
+string bullpgia::calculateBullAndPgia(string choice, string guess) {
+
+   int length_choice = choice.length();
+   char *arr_temp = new  char[length_choice];
+   for (int q=0;q<length_choice;q++)
+   {
+       arr_temp[q]=0;
+   }
+
+    int count_bull = 0; 
+    int count_pgia = 0; 
+
+    for (int k = 0; k < length_choice; k++)
+    {
+        if (guess[k] == choice[k])   
+        {
+             arr_temp[k] = 'b';   continue ;
+
+        }
+        
+    
+    }
+
+    for (int a = 0; a < 10; a++) 
+    {
+        for (int i = 0; i < length_choice ; i++) 
+        {
+            if ((guess[i] == 48 + a) && (arr_temp[i] !='b' )) 
+            {
+                for (int j = 0; j < length_choice; j++)
+                {
+                    if ( (choice[j] == guess[i]) && (i != j))
+                    {
+                        if (arr_temp[j] == 0)
+                        {   
+                            arr_temp[j] = 'p'; 
+                            break;
+    
 			}
-			for (j = 0; j < guess.length(); j++) {
-				g = guess.at(j);
-				if (c == g && arr_ZeroF_oneT[j]==0) {
-					pgiaa++;
-					arr_ZeroF_oneT[j] = 1;
-					break;
-				}
-
-			}
+		    }
 		}
+	    }
+	}
+    }
 
-		delete[] arr_ZeroF_oneT;
-		string cal = to_string(bulls) + "," + to_string(pgiaa);
-    return cal;
-}
+
+
+    //// COUNT PGIA AND BULL ////
+
+
+    for (size_t i = 0; i < length_choice; ++i)
+    {
+        count_pgia = count_pgia+ (arr_temp[i] == 'p');
+        count_bull = count_bull+ (arr_temp[i] == 'b');  
+    }
+     delete[] arr_temp;
+    // std::cout << to_string(count_bull) + "," + to_string(count_pgia) << std::endl;
+    return to_string(count_bull) + "," + to_string(count_pgia);
 }
